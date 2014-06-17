2f552ad34131dfe2005ced2626083ba4ed8a18d2
freennnnn <freennnn@gmail.com>
2014-06-17 11:27:48 -0500

    Initial Commit

---
+++ CardDeck/APAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APAppDelegate.h
+//  CardDeck
+//
+//  Created by alexander.pranevich on 6/17/14.
+//  Copyright (c) 2014 Alexander Pronevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardDeck/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardDeck
+//
+//  Created by alexander.pranevich on 6/17/14.
+//  Copyright (c) 2014 Alexander Pronevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APAppDelegate class]));
+    }
+}
